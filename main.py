dddbb202e92a2ac888260385fcad088543bc53ab
Allison Boukall <boukall@cs.ualberta.ca>
2019-01-08 09:47:56 -0700

    question 8 fix

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ print(requests.__version__)
 r = requests.get("http://www.google.com")
 print (r.status_code)
 print(r.headers)
+
+github_url = "https://raw.github.com/sewingpillows/labs/lab1/main.py"
+r = requests.get(github_url)
+print(r.text)
